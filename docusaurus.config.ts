df9b9707c6442cb5b4f185c7e0183cd1bb67dd34
AlexV <a.viarvelskii@cdek.ru>
2025-07-23 16:34:44 +0700

    feat: initial

---
+++ docusaurus.config.ts
@@ -0,0 +1,147 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+const config: Config = {
+  title: 'CDEK Yandex.Market',
+  tagline: 'Yandex + CDEK = ❤️',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://cdek-it.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/yande/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'cdek-it', // Usually your GitHub org/user name.
+  projectName: 'yandex', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'ru',
+    locales: ['ru'],
+  },
+
+  plugins: [
+      [
+        '@docusaurus/plugin-ideal-image',
+        {},
+      ],
+      [
+        'docusaurus-plugin-yandex-metrica',
+        {
+          counterID: '101134743',
+          webvisor: true,
+        }
+      ],
+    async function flomni(context, options) {
+      return {
+        name: 'flomni',
+        injectHtmlTags() {
+          return {
+            headTags: [
+              {
+                tagName: 'script',
+                innerHTML: `window.flomniConfig={clientKey:"${process.env.FLOMNI_CLIENT}",threadId:"${process.env.FLOMNI_THREAD}"}`,
+              },
+            ],
+            postBodyTags: [
+              {
+                tagName: 'div',
+                attributes: {'id': 'flomni'},
+              },
+              {
+                tagName: 'script',
+                attributes: {'src': 'https://i.v2.flomni.com/chat.corner.js'},
+              },
+            ]
+          };
+        },
+      };
+    }
+  ],
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          routeBasePath: '/',
+          sidebarPath: './sidebars.json',
+        },
+        blog: false,
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/banner.png',
+    navbar: {
+      hideOnScroll: true,
+      title: 'Wildberries',
+      logo: {
+        alt: 'Логотип CDEK',
+        src: 'img/logo.svg',
+      },
+      items: [
+        {
+          type: 'localeDropdown',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'light',
+      links: [
+        {
+          title: 'Официальные ресурсы',
+          items: [
+            {
+              label: 'Заключить договор',
+              href: 'https://www.cdek.ru/ru/contract/',
+            },
+            {
+              label: 'Личный кабинет',
+              href: 'https://lk.cdek.ru/',
+            },
+          ],
+        },
+        {
+          title: 'Полезные ссылки',
+          items: [
+            {
+              label: 'Рассчитать стоимость доставки',
+              href: 'https://www.cdek.ru/ru/',
+            },
+            {
+              label: 'Техническая поддержка',
+              href: 'mailto:integrator@cdek.ru',
+            },
+          ],
+        },
+      ],
+      copyright: `Copyright © 2025 ООО "СДЭК ДИДЖИТАЛ"`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+      defaultLanguage: 'php',
+      magicComments: [],
+      additionalLanguages: [],
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+// noinspection JSUnusedGlobalSymbols
+export default config;
